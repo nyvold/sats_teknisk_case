8af904eda07f48da09a8f3db58095595896af22f
Jonas Berger Nyvold <123556671+nyvold@users.noreply.github.com>
2024-10-03 23:37:38 +0200

    Update searchResultHolder.py

--- searchResultHolder.py
+++ searchResultHolder.py
@@ -12,30 +12,20 @@ class searchResultHolder:
     def add_results(self, result: Iterable[dict]) -> None:
         #tar inn et hashmap og lager formaterte resultat objekter fra hashmappet
         #og legger det inn i results listen
-        for result in self.__get_results(result):
+        for result in self.__retrive_results(result):
             formatted_result = searchResult(result)
             self.results.append(formatted_result)
 
 
-    def __get_results(self, results: json) -> Iterable[dict]:
+    def __retrive_results(self, results: json) -> Iterable[dict]:
         #tar filen og yielder/"returerer" tilbake noe itererbart
         with open(results, "r") as file:
             data = json.load(file)
         results_iterable = data["results"]#litt hardkodet muligens, enn hvis json filen har noe annet enn "results"
         
         yield from results_iterable
-        
 
 
+    def get_results(self) -> Iterable[searchResult]:
+        yield from self.results
 
-#teste litt
-def main():
-    result_holder = searchResultHolder()
-    result_holder.add_results(sys.argv[1])
-    for i, result in enumerate(result_holder.results):
-        print(f"{i+1}")
-        print(result)
-        print("\n\n")
-    
-if __name__ == "__main__":
-    main()
